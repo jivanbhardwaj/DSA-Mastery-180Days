ecd86b9134244304bfb2c58ec3d497cb07131fc2
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-09 00:55:06 +0530

    c++ skeleton

---
+++ day01_basics.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h> // Include all library
+using namespace std; //Include I/O Library 
+int main(){         // Entry point where c++ consider to read your code
+cout<<"Basic skeleton"<<endl;
+    return 0;
+    
+}
