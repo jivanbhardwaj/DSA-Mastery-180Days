3af5b22e16c96dea5628c1a05626774deedb9c4b
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-09 00:11:48 +0530

    cpp basic concept practice day-01

--- basics.cpp
+++ basics.cpp
@@ -1,7 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"day_01_cpp_setup_vscode"<<endl;
-    cout<<"Set the git repo to push all upcoming dsa code"<<endl;
+     //basic skeleton
+
+    //data type
+
+    //Input-output
+
+    //if else 
+
+    //for loop
+
+    //while loop
+
+    //switch
+
+    //array
+
+    //string 
+
+    //fucntion
+
+    //pass by value and reference
     return 0;
 }
