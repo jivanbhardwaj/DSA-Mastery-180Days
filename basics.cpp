f10f942c432bfb09a33bfe4c76719e24d3b7289f
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-08 23:44:22 +0530

    cpp_setup"

--- basics.cpp
+++ basics.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"cpp_setup_day_01"<endl;
+    cout<<"day_01_cpp_setup_vscode"<<endl;
     cout<<"Set the git repo to push all upcoming dsa code"<<endl;
     return 0;
 }
