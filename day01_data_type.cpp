d50a29cead9ce3e0d2d43cb11dd92c4f463317ef
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-09 09:22:14 +0530

    data type int,long,llong,float,double,and string

---
+++ day01_data_type.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //datatype
+    int int_min= INT_MIN;
+    int int_max= INT_MAX;
+    // int can store -10^9 to 10^9
+    // long can store -10^12 t0 10^12
+    // lomg long can store -10^18 to 10^18
+    long long_num=LONG_MIN;
+    long numlong=LONG_MAX;
+    long long ll_max=LLONG_MAX;
+    long long ll_min=LLONG_MIN;
+    cout<<int_min<<endl<<int_max<< endl<<long_num<<endl<<numlong<<endl<<ll_max<<endl<<ll_min<<endl;
+    return 0;
+}
