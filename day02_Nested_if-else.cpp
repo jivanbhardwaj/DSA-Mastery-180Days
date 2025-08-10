458cceeccdbe21789b468f40b236ea5ef507aae3
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-11 01:14:40 +0530

    Nested loop

--- day02_Nested_if-else.cpp
+++ day02_Nested_if-else.cpp
@@ -5,17 +5,17 @@ int main(){
     cin>>a>>b>>c;
     if (a>=b){
         if (a>=c){
-            cout<<"a is the largest:", a;
+            cout << "a is the largest: " << a;
         }
     }
 // In this above case if a greater than b and c then obviously a will be the largest
     else if (b>=c){
-            cout<<"b is the largest:", b;
+            cout << "b is the largest: " << b;
         }
 /* In above block there is no need to compare if b is greater than a or not, beacuse from first block it proven that 
 if a is not greater than b then b will greater than a and if b is greater than c as well then b will the largest*/
     else{
-            cout << "c is the largest:", c;
+            cout << "c is the largest: " << c;
         }
 // if both above cases is false then c will be the largest.
     }
