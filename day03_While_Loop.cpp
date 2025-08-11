7c211963a01527e3270b6fe21a4e4c9579c99e8a
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-11 23:24:42 +0530

    while loop

---
+++ day03_While_Loop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string city;
+    int count = 1;
+    getline(cin, city); // Read a line of input including spaces
+   while(count<=10){
+        cout << city << endl; 
+        count++;
+        
+    }
+    return 0;
+   }
