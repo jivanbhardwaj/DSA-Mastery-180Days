201fc449955faf4898eb6b5fa75ebb3939fe812e
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-12 20:46:41 +0530

    Array element Taken by cin

---
+++ day04_Array_2nd.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int bob[5]; // Array of integers
+    for(int i = 0; i <= 4; i++) {
+        cin >> bob[i]; // Input each element of the array
+        cout << bob[i] << endl;
+    }
+    return 0;
+}
