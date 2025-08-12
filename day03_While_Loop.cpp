91abc59de9bad007f61efbd146b3f1cf0d66a10c
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-12 06:36:48 +0530

    While loop

--- day03_While_Loop.cpp
+++ day03_While_Loop.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * @brief Reads a city name from user input and prints it 10 times.
+ *
+ * The program prompts the user to enter a city name (which may include spaces),
+ * then outputs the entered city name to the console 10 times, each on a new line.
+ *
+ * @return int Returns 0 upon successful completion.
+ */
 int main() {
     string city;
     int count = 1;
