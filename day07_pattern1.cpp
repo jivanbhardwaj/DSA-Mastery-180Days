0e62a0bb9cf459964edba75e39bc827c38d9ca8c
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-15 13:48:17 +0530

    star pattern n*n

--- day07_pattern1.cpp
+++ day07_pattern1.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 /*
- * The function uses nested loops to print 4 rows, each containing 4 asterisks ('*').
+ * The function uses nested loops to print n rows, each containing 4 asterisks ('*').
  * After printing each row, it moves to the next line.
  *
  * int Returns 0 upon successful completion.
  */
 int main() {
-    for (int row = 1; row <= 4; row++) {
-        for (int col = 1; col <= 4; col++) {
+    int n;
+    cin >> n; // Read the number of rows from input
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= n; col++) {
         cout << "*"; // Print a star for each column in the current row
         }
         cout << endl; // Move to the next line after printing stars for the current row
