40436c4c9175efdf90eb38bf4094ba4f8c66fd05
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-16 09:57:43 +0530

    Pettern-5 right-angled triangle pattern of asterisks with decreasing rows

--- day08_pattern5.cpp
+++ day08_pattern5.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 /**
  * Prints a right-angled triangle pattern of asterisks with decreasing rows.
  * 
  * The program reads an integer 'n' from standard input and prints 'n' rows.
- * Each row starts with 'i' (from 1 to n) and prints asterisks from 'i' to 'n'.
+ * Each row contains asterisks, starting from 'n' asterisks in the first row
+ * and decreasing by one asterisk in each subsequent row.
  * For example, if n = 4, the output will be:
  * ****
  * ***
  * **
  * *
  */
+
 int main() {
     int n;
     cin >> n;
     
     for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
+        for (int j = 1; j <= n-i+1; j++) {
             cout << "*";
         }
         cout << endl;
