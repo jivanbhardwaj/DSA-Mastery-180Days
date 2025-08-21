3c5fb55b649a7457ed0901012ad570d670c01289
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-21 16:18:36 +0530

    Number Triangle Pattern

---
+++ day11_pattern13.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+1
+23
+456
+78910
+
+*/
+
+int main() {
+    int num=1;
+    int i, j, n;
+    cin >> n;  // Read the height of the pattern    
+    for (i = 1; i <= n; i++) {
+        // Print increasing numbers
+        for (j = 1; j <= i; j++) {
+            cout << num;
+            num++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
