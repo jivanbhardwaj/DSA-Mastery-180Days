7b291aff959031ea115a9d9450d296fc505d7e39
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-22 13:43:22 +0530

    Left-aligned Right Triangle Star Pattern

--- day07_pattern2.cpp
+++ day07_pattern2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /**
- * 
+
  * This function reads an integer 'n' from standard input and prints a left-aligned 
  * right-angled triangle pattern of asterisks. The triangle has 'n' rows, where the 
  * first row contains one asterisk, the second row contains two, and so on, up to 'n' asterisks 
