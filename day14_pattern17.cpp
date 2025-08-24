2e3b04e6bf09c2ec54a6d2acd85f65e54861500e
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-25 00:05:13 +0530

    Alphabet Palindromic Pyramid — a centered palindromic triangle

---
+++ day14_pattern17.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* 
+A
+ABA
+ABCBA
+ABCDCBA
+ABCDEDCBA
+
+
+*/
+int main() {
+    int n;
+    cin >> n;   
+        // Outer loop for the number of rows.
+        for(int i = 0; i < n; i++) {
+            
+            // Printing spaces before characters.
+            for(int j = 0; j < n - i - 1; j++) {
+                cout << " ";
+            }
+            
+            // Printing characters.
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
+            for(int j = 1; j <= 2 * i + 1; j++) {
+                cout << ch;
+                if(j <= breakpoint)
+                    ch++;
+                else
+                    ch--;
+            }
+            
+            // Move to the next line for the next row.
+            cout << endl;
+        }
+    }
