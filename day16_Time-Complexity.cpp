bf31d77458f435a071e8bb555682a1fd696345f0
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-31 10:28:53 +0530

    Time Complexity

---
+++ day16_Time-Complexity.cpp
@@ -0,0 +1,22 @@
+/*
+Time Complexity
+
+Time complexity is a computational complexity that describes the rate of change of input size to the time taken by an algorithm.
+
+
+Common time complexities (Big O notation):
+1. O(1) - Constant time
+2. O(log n) - Logarithmic time
+3. O(n) - Linear time
+4. O(n log n) - Linearithmic time
+5. O(n^2) - Quadratic time
+6. O(2^n) - Exponential time
+7. O(n!) - Factorial time
+
+Understanding time complexity is crucial for writing efficient algorithms and optimizing code performance.
+
+But there are mainly three time complexities to consider:
+1. Best Case (Omega)
+2. Average Case(Theta)
+3. Worst Case (Big O)
+*/
