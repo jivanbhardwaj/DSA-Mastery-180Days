cd05e474e40cd9130e05ae20960e70a56bf0c272
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-31 10:33:37 +0530

    Space Complexity

---
+++ day16_Space_Complexity.cpp
@@ -0,0 +1,10 @@
+/*
+
+Space complexity = Auxiliary Space + Input Space
+
+Auxiliary Space: Extra space or temporary space used by an algorithm or Space use by code.
+Input Space: Space taken by the inputs to the algorithm.
+
+Note- But while calculating space complexity, we usually consider the auxiliary space and ignore the input space.
+
+*/
