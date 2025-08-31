50ab544890a0def78aa6a6c2b622776459abe1f7
Jivan Bhardwaj <jivan.6b@gmail.com>
2025-08-31 10:57:18 +0530

    Pair Class in STL

---
+++ day17_STL.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void explainPair(){
+    pair<int, int> p1={1, 2};
+    pair<char, int> p2={'n', 2};
+    cout<<p1.first<<" "<<p1.second<<endl;
+    cout<<p2.first<<" "<<p2.second<<endl;
+}
+
+int main() {
+    explainPair();
+    return 0;
+}
